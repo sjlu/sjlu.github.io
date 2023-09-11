24325838369ec3ef110b96506fc7b560d26e6c73
Steven Lu <tacticalazn@gmail.com>
2023-09-11 15:53:20 -0400

    Adding Contentful

---
+++ download-contentful.js
@@ -0,0 +1,112 @@
+const Promise = require('bluebird')
+const contentful = require('contentful')
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+})
+
+function convertEntryItemsToPosts (entry) {
+  const fields = entry.fields ?? {}
+
+  const title = fields.title
+  const description = fields.description
+  const slug = fields.slug
+  const date = fields.date
+  let body = fields.body
+
+  const images = body.match(/\/\/(assets|images).ctfassets.net\/.+\/.+\/.+\/.+\.(heic|png|jpeg|jpg|webp)/ig)
+  const replacedImages = []
+  for (const image of images) {
+    const url = image.match(/\/\/(assets|images).ctfassets.net\/(.+)\/(.+)\/(.+)\/(.+)\.(heic|png|jpeg|jpg|webp)/i)
+
+    const imageUrl = `https://images.ctfassets.net/${url[2]}/${url[3]}/${url[4]}/${url[5]}.${url[6]}?fm=webp&w=1600`
+    const folder = `/images/${slug}`
+    const filename = `${url[5]}.webp`
+
+    body = body.replace(image, `${folder}/${filename}`)
+
+    replacedImages.push({
+      url: imageUrl,
+      folder,
+      filename
+    })
+  }
+
+  return {
+    title,
+    description,
+    slug,
+    date,
+    body,
+    images: replacedImages
+  }
+}
+
+async function fileExists (path) {
+  return fs.promises.access(path, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false)
+}
+
+async function downloadImage (image) {
+  const folder = path.join(__dirname, image.folder)
+  const imagePath = path.join(__dirname, `${image.folder}/${image.filename}`)
+
+  if (await fileExists(imagePath)) {
+    return
+  }
+
+  await fs.promises.mkdir(path.join(__dirname, image.folder), { recursive: true }).catch(console.error)
+
+  const resp = await axios({
+    url: image.url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  return new Promise ((resolve, reject) => {
+    resp.data.pipe(fs.createWriteStream(imagePath))
+      .on('error', reject)
+      .once('close', resolve)
+  })
+}
+
+async function writePost (post) {
+  const content = `---
+layout: post
+title: "${post.title}"
+description: "${post.description}"
+tags: []
+---
+
+${post.body}`
+
+  const postPath = path.join(__dirname, `posts/_posts/${post.date}-${post.slug}.md`)
+  await fs.promises.writeFile(postPath, content)
+}
+
+async function getPosts () {
+  const postItems = await getEntryItems('post')
+  const posts = []
+  for (const postItem of postItems) {
+    posts.push(convertEntryItemsToPosts(postItem))
+  }
+
+  const images = []
+  for (const post of posts) {
+    for (const image of post.images) {
+      images.push(image)
+    }
+  }
+  await Promise.resolve(images).map(downloadImage, { concurrency: 10 })
+  await Promise.resolve(posts).map(writePost, { concurrency: 10 })
+}
+
+async function run () {
+  await getPosts()
+}
+
+run()
--- posts/_posts/2023-08-24-networking-with-unifi.md
+++ posts/_posts/2023-08-24-networking-with-unifi.md
@@ -1,6 +1,6 @@
 ---
 layout: post
-title: "Networking with Unifi"
+title: "Networking with UniFi"
 description: "I put a rack mounted switch into my apartment"
 tags: []
 ---
@@ -9,7 +9,7 @@ As one of my side projects, I wanted to get serious about my network. I went a l
 
 ![](/images/networking/IMG_0371.webp)
 
-So the first thing I purchased right off the bat was the Unifi Dream Machine SE (UDM SE). This includes 2 PoE+ and 6 PoE switch ports at 1 Gbps.
+So the first thing I purchased right off the bat was the UniFi Dream Machine SE (UDM SE). This includes 2 PoE+ and 6 PoE switch ports at 1 Gbps.
 
 Attached to the ports are 4 access points in each of my apartment's rooms. You ask, why do you need so many? It's because most of my walls are cement and in NYC, 5 GHz really only works well with line of sight. Those 4 access points consist fo 2x U6 In-Wall and 2x U6 Professional. I personally wish I went with all In-Walls since my aparment is pre-wired and doesn't have any ceiling mounts. You live and you learn.
 
@@ -17,10 +17,10 @@ Next up, making it look nice. I could have purchased a rack but it would take qu
 
 ![](/images/networking/IMG_0426.webp)
 
-After some additional devices, I quickly needed to add additional ports. To do so, I decided to add the Unifi Enterprise 24 PoE which has PoE+ on all ports and 12 2.5 GbE. To get it hooked up with the UDM SE, there is a 10 Gbps SFP+ DAC cable connecting them together.
+After some additional devices, I quickly needed to add additional ports. To do so, I decided to add the UniFi Enterprise 24 PoE which has PoE+ on all ports and 12 2.5 GbE. To get it hooked up with the UDM SE, there is a 10 Gbps SFP+ DAC cable connecting them together.
 
 ![](/images/networking/IMG_0628.webp)
 
 With that, the networking project is complete and there are enough ports to expand to build a home lab very soon.
 
-P.S. An added extra benefit with Unifi: cameras record locally onto the UDM SE.
+P.S. An added extra benefit with UniFi: cameras record locally onto the UDM SE.
